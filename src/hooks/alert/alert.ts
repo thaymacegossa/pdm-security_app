35ccbd4f8edaca4dd075f7f354ee07062cd3a95d
Deborah <thaymacegossa@gmail.com>
2026-05-31 23:07:27 -0300

    Create alert.ts
    Criação do hook de alert, aguardando a implementação do store do usuário.

---
+++ src/hooks/alert/alert.ts
@@ -0,0 +1,26 @@
+import { saveAlert } from "@/src/services/firebase/alert.service";
+import { getCurrentLocation } from "@/src/services/location.service";
+import { devLog } from '@utils/dev-log';
+
+
+export async function alertTrigger() {
+    try {
+        const location = await getCurrentLocation();
+        const address = location?.address || "Unknown Location";
+        const geolocation = {
+            latitude: location?.latitude || 0,
+            longitude: location?.longitude || 0,
+        };
+
+        const triggerAlert = await saveAlert("userId", {
+            actualAlert: true,
+            geolocation,
+            location: address,
+        });
+        devLog("[useSosTrigger] alerta salvo no Firebase", { triggerAlert });
+    } catch (error) {
+        devLog("[useSosTrigger] erro ao salvar alerta no Firebase", { error });
+    }
+
+    return location;
+}
