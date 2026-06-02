7422c18b4b5d06c12c534b2cd95d94e3a59556d6
Deborah <thaymacegossa@gmail.com>
2026-06-01 22:44:38 -0300

    Ajuste de ícones de tab bar
    Substituição da biblioteca do FontAwesome para FontAwesome5 e ajuste de cor dos ícones, corrigindo problema de exibição em aparelhos Android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -19,6 +20,7 @@ export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
+    ...FontAwesome5.font,
   });
 
   useEffect(() => {
